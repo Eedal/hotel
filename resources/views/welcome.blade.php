7987fb06c5cd6bc15188f7786a7c18655e2816f9
Luis Ospino <lfospinoayala@gmail.com>
2019-09-29 11:31:23 -0500

    actualizando titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Entorno de Luis Ospino
                 </div>
 
                 <div class="links">
