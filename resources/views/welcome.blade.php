83cc7b6e76ce7813f240888e7cd67b9116925d1e
Luis Ospino <lfospinoayala@gmail.com>
2019-09-29 11:16:05 -0500

    modificacion del titulo desde el master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Luis Ospino Modificando
                 </div>
 
                 <div class="links">
