56b0351ab7d5456174c562ef28e85fdf2ab81294
Luis Ospino <lfospinoayala@gmail.com>
2019-09-29 11:32:11 -0500

    actualizando titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Luis Ospino Modificando
+                    Luis Ospino Modificando (RAMA MASTER)
                 </div>
 
                 <div class="links">
